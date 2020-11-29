f917d7be245bfbcafe5f6113bbfe3dd74694918c
Protsenko Arseny <procenko_03@mail.ru>
2020-11-29 10:31:12 +0500

    added account_recognition.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,7 +1,7 @@
 # TODO below is for mock only
 
 from random import choice
+from .account_recognition imprort recognize_profile, AccountFinder, AccountRecognizer
 
-
-def get_matched_image(target, images):
-    return choice(images)
+#def get_matched_image(target, images):
+#    return choice(images)
